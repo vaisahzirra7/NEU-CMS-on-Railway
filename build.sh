c216e6297847b7f47baede60f8d2ca2ad88582fa
Daniel Ojo <dojo8702@gmail.com>
2026-03-10 14:05:22 +0100

    Deployment configs: load data commands

--- build.sh
+++ build.sh
@@ -5,3 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically load the database dump if it exists
+if [ -f data.json ]; then
+    echo "Found data.json. Loading data into the database..."
+    python manage.py loaddata data.json
+    
+    # Rename the file so it doesn't get loaded again on subsequent deployments
+    # (or if Render crashes and restarts the instance).
+    mv data.json data_loaded.json
+    echo "Data loaded successfully and file renamed to prevent duplicate loads."
+fi
