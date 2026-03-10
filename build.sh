6d59e16b45414bda825dd408e9413bb26ac7d835
Vaisah Peter Zirra <vaisahzirra7@gmail.com>
2026-03-10 16:53:15 +0100

    PReview Fix

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_superuser_env --noinput
+python manage.py create_superuser_env
 
 # Automatically load the database dump if it exists
 if [ -f data.json ]; then
