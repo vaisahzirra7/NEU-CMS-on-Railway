79d2e2db50b355ab93bd8a8a54bf3110a70dda24
Vaisah Peter Zirra <vaisahzirra7@gmail.com>
2026-03-10 16:34:49 +0100

    Login added Logo

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # build.sh — Render build script
 
 # Start of code to extract the content of the database
+# Run this code in your local environment to create a data.json file with the content of your database, then copy that file to the root of this project before deploying to Render.
 # ---------------------------------------
 # set "DJANGO_DB_ENGINE=django.db.backends.mysql"
 # set "DJANGO_DB_NAME=your_local_db"
@@ -12,7 +13,11 @@
 
 # python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE','core.settings'); django.setup(); from django.core.management import call_command; call_command('dumpdata','--exclude','auth.permission','--exclude','contenttypes','--exclude','admin.logentry', stdout=open('data.json','w', encoding='utf-8'))"
 # ----------------------------------------
+
+
 # End of code to extract the content of the database
+
+
 set -o errexit
 
 pip install -r requirements.txt
