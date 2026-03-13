9c5935b0c27d2993ae9c98c8d9c059fa6759b5bd
Vaisah Peter Zirra <vaisahzirra7@gmail.com>
2026-03-13 01:57:50 +0100

    Changes by VEE

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env bash
 # build.sh — Render build script
 
+#!/usr/bin/env bash
+
+
+
+#---------------------------------------
 # Start of code to extract the content of the database
 # Run this code in your local environment to create a data.json file with the content of your database, then copy that file to the root of this project before deploying to Render.
 # ---------------------------------------
+
 # set "DJANGO_DB_ENGINE=django.db.backends.mysql"
 # set "DJANGO_DB_NAME=your_local_db"
 # set "DJANGO_DB_USER=your_user"
@@ -12,10 +17,19 @@
 # set "DJANGO_DB_PORT=3306"
 
 # python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE','core.settings'); django.setup(); from django.core.management import call_command; call_command('dumpdata','--exclude','auth.permission','--exclude','contenttypes','--exclude','admin.logentry', stdout=open('data.json','w', encoding='utf-8'))"
+
+
 # ----------------------------------------
-
-
 # End of code to extract the content of the database
+#----------------------------------------   
+
+
+
+#----------------------------------------
+# Simpler version to migrrate MySQL to PostgreSQL
+#----------------------------------------   
+# python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE','core.settings'); django.setup(); from django.core.management import call_command; call_command('dumpdata','--exclude','auth.permission','--exclude','contenttypes','--exclude','admin.logentry', stdout=open('data.json','w', encoding='utf-8'))"
+
 
 
 set -o errexit
